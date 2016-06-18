f6a9ffa3bd5b54c8984bb50a5d241bcbc8059269
dohoangphuong <hp.codoc@yahoo.com.vn>
2016-06-18 15:13:31 +0700

    +Add: Chapter05.2: Lấy ấy ảnh từ album       Chapter05.3: Ghép nối sử dụng camera với lấy ảnh từ album vào 1 source

---
+++ Chapter05.2-Open Image/MainActivity.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Util;
+
+namespace Chapter05._2_Open_Image
+{
+    [Activity(Label = "Chapter05._2_Open_Image", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.myButton);
+            Log.Warn("aaaaaaaaaaaaaaaaaaaaaaaaaaaa", "A0");
+            button.Click += delegate {
+                Log.Warn("aaaaaaaaaaaaaaaaaaaaaaaaaaaa", "A011");
+                var imageIntent = new Intent();
+                imageIntent.SetType("image/*");
+                imageIntent.SetAction(Intent.ActionGetContent);
+                StartActivityForResult(
+                    Intent.CreateChooser(imageIntent, "Select photo"), 0);
+            };
+            Log.Warn("aaaaaaaaaaaaaaaaaaaaaaaaaaaa", "A01");
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            Log.Warn("aaaaaaaaaaaaaaaaaaaaaaaaaaaa", "A1");
+            base.OnActivityResult(requestCode, resultCode, data);
+
+            if (resultCode == Result.Ok)
+            {
+                Log.Warn("aaaaaaaaaaaaaaaaaaaaaaaaaaaa", "A2");
+                var imageView =
+                    FindViewById<ImageView>(Resource.Id.myImageView);
+                imageView.SetImageURI(data.Data);
+            }
+        }
+    }
+}
+
---
+++ Chapter05.3-Take photo/MainActivity.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Widget;
+using Android.OS;
+using Android.Provider;
+using Android.Content.PM;
+using System.Collections.Generic;
+using Android.Graphics;
+using Java.IO;
+using Android.Util;
+
+namespace Chapter05._3_Take_photo
+{
+    [Activity(Label = "Take Photo", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        ImageView _imageView;
+        bool openImage = false, usingCameraBool = false;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            //Using Camera
+            if (IsThereAnAppToTakePictures())
+            {
+                CreateDirectoryForPictures();
+                Button UsingCamera = FindViewById<Button>(Resource.Id.UsingCamera);
+                _imageView = FindViewById<ImageView>(Resource.Id.MyImage);
+                UsingCamera.Click += TakeAPicture;
+            }
+
+            //Choose photo
+            Button choose = FindViewById<Button>(Resource.Id.OpenImage);
+            choose.Click += delegate
+            {
+                var imageIntent = new Intent();
+                imageIntent.SetType("image/*");
+                imageIntent.SetAction(Intent.ActionGetContent);
+                StartActivityForResult(
+                    Intent.CreateChooser(imageIntent, "Select photo"), 0);
+                openImage = true;
+            };
+
+        }
+
+        private void CreateDirectoryForPictures()
+        {
+            App._dir = new File(
+                Android.OS.Environment.GetExternalStoragePublicDirectory(
+                    Android.OS.Environment.DirectoryPictures), "CameraAppDemo");
+            if (!App._dir.Exists())
+            {
+                App._dir.Mkdirs();
+            }
+        }
+
+        private bool IsThereAnAppToTakePictures()
+        {
+            Intent intent = new Intent(MediaStore.ActionImageCapture);
+            IList<ResolveInfo> availableActivities =
+                PackageManager.QueryIntentActivities(intent, PackageInfoFlags.MatchDefaultOnly);
+            return availableActivities != null && availableActivities.Count > 0;
+        }
+
+        private void TakeAPicture(object sender, EventArgs eventArgs)
+        {
+            Intent intent = new Intent(MediaStore.ActionImageCapture);
+            App._file = new File(App._dir, String.Format("myPhoto_{0}.jpg", Guid.NewGuid()));
+            intent.PutExtra(MediaStore.ExtraOutput, Android.Net.Uri.FromFile(App._file));
+            StartActivityForResult(intent, 0);
+            usingCameraBool = true;
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            ////Choose Image
+            if (openImage == true && resultCode == Result.Ok)
+            {
+                openImage = false;
+                ImageView imageView = FindViewById<ImageView>(Resource.Id.MyImage);
+                imageView.SetImageURI(data.Data);             
+            }
+
+            // Make it available in the gallery
+            if (usingCameraBool==true)
+            {
+                usingCameraBool = false;
+                Intent mediaScanIntent = new Intent(Intent.ActionMediaScannerScanFile);
+                Android.Net.Uri contentUri = Android.Net.Uri.FromFile(App._file);
+                mediaScanIntent.SetData(contentUri);
+                SendBroadcast(mediaScanIntent);
+
+                // Display in ImageView. We will resize the bitmap to fit the display.
+                // Loading the full sized image will consume to much memory
+                // and cause the application to crash.
+
+                int height = Resources.DisplayMetrics.HeightPixels;
+                int width = _imageView.Height;
+                App.bitmap = App._file.Path.LoadAndResizeBitmap(width, height);
+                if (App.bitmap != null)
+                {
+                    _imageView.SetImageBitmap(App.bitmap);
+                    App.bitmap = null;
+                }
+
+                // Dispose of the Java side bitmap.
+                GC.Collect();
+            }
+        }
+    }
+
+    public static class BitmapHelpers
+    {
+        public static Bitmap LoadAndResizeBitmap(this string fileName, int width, int height)
+        {
+            // First we get the the dimensions of the file on disk
+            BitmapFactory.Options options = new BitmapFactory.Options { InJustDecodeBounds = true };
+            BitmapFactory.DecodeFile(fileName, options);
+
+            // Next we calculate the ratio that we need to resize the image by
+            // in order to fit the requested dimensions.
+            int outHeight = options.OutHeight;
+            int outWidth = options.OutWidth;
+            int inSampleSize = 1;
+
+            if (outHeight > height || outWidth > width)
+            {
+                inSampleSize = outWidth > outHeight
+                                   ? outHeight / height
+                                   : outWidth / width;
+            }
+
+            // Now we will load the image and have BitmapFactory resize it for us.
+            options.InSampleSize = inSampleSize;
+            options.InJustDecodeBounds = false;
+            Bitmap resizedBitmap = BitmapFactory.DecodeFile(fileName, options);
+
+            return resizedBitmap;
+        }
+    }
+
+    public static class App
+    {
+        public static File _file;
+        public static File _dir;
+        public static Bitmap bitmap;
+    }
+}
